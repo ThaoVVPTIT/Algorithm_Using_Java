57c04ccee945601c6bed4f53b01c0c863b41abbf
ThaoVVPTIT <thaoptit0102@gmail.com>
2024-10-22 14:57:37 +0700

    Day3 Basic Link list single

---
+++ Day3_LinkListSigle/NodeSigle/src/cdac/node/SLNodeMain.java
@@ -0,0 +1,16 @@
+package cdac.node;
+
+public class SLNodeMain {
+
+	public static void main(String[] args) {
+		SortedList list = new SortedSigleList();
+		list.insert(12);
+		list.insert(134);
+		list.insert(122);
+		list.insert(121);
+		
+		list.print();
+
+	}
+
+}
